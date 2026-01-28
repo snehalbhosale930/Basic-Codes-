72cfecb25fc62eaa5139173195923c5afd97add0
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-01-28 23:27:54 +0530

    Add files via upload
    This is Math function File.

---
+++ MathFunction.cs
@@ -0,0 +1,24 @@
+﻿
+namespace BasicCodes
+{
+    internal class MathFunction
+    {
+       public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int sub(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Mul(int a,int b, int c)
+        {
+            return a * b + c;
+        }
+        public static int Div(int a, int b, int c)
+        {
+            return a / b + c;
+        }
+    }
+}
+ 
