0e0cc49f02d55dfab1a4ff87f1d497995052fe7f
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-01-28 23:30:27 +0530

    Add files via upload
    This is Factorial class file.

---
+++ Fact.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace BasicCodes
+{
+    internal class Fact
+    { 
+        public static void Num(int a)
+        {
+            int count = 1;
+            if (a>0)
+            {
+                while (a>0)
+                {
+                    count = count * a;
+                    a = a - 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Factorial of " + a + "is :" + count);
+            }
+            Console.WriteLine("Factorial of " + a + "is :" + count);
+        }
+    }
+}
