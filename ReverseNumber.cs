f7ebf18e52543a21dd81fb2c7862b7eb39b9911a
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-01-29 22:36:40 +0530

    Add files via upload
    This is the Reverse The Number Program File.

---
+++ ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BasicCodes
+{
+    internal class ReverseNumber
+    {
+        public static void ReverseTheNumber()
+        {
+            int Num, temp, rev = 0;
+            Console.WriteLine("Enter The Number");
+            Num = Convert.ToInt32(Console.ReadLine());
+            temp = Num;
+            while (temp > 0)
+            {
+                int digit = Num % 10;
+                rev = rev * 10 + digit;
+                Num = Num / 10;
+            }
+            Console.WriteLine("Reverse Number:" + rev);
+            if (temp == rev)
+                Console.WriteLine("The Number Is Palindrome");
+            else
+                Console.WriteLine("The Number Is Not Palindrome");
+        }
+    }
+}
