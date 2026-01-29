fc624eec4c4da235b9491fdecca47282c2f133a7
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-01-29 22:29:48 +0530

    Add files via upload
    This is the Addition, Substraction, Multiplication and Division program File.

---
+++ AddSubMulDiv.cs
@@ -0,0 +1,22 @@
+﻿namespace BasicCodes
+{
+    internal class AddSubMulDiv
+    {
+        public static void cal()
+        {
+            int a, b;
+            a = 30;
+            b = 20;
+            int c = a + b;
+            int d = a - b;
+            int e = a * b;
+            int f = a / b;
+            Console.WriteLine("Addition is:" + c);
+            Console.WriteLine("Substraction is:" + d);
+            Console.WriteLine("Multiplication is:" + e);
+            Console.WriteLine("Division is:" + f);
+            Console.ReadLine();
+
+        }
+    }
+}
