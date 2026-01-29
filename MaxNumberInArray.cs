2c96db2bafd2d13b200144ee533962b7197e28e6
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-01-29 22:31:12 +0530

    Add files via upload
    This is the Find Maximum Number In Array program File.

---
+++ MaxNumberInArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BasicCodes
+{
+    internal class MaxNumberInArray
+    {
+        public static void MaxNumInArray()
+        {
+            Console.WriteLine("Enter The Size:");
+            int Length = Convert.ToInt32 (Console.ReadLine());
+
+            int[] array = new int[Length];
+            for (int i = 0; i < Length; i++) 
+            {
+                Console.WriteLine("Enter the element:");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int max = array[0];
+            for(int i = 1; i < Length; i++) 
+            {
+                if(max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+            Console.WriteLine("Greater Number is:" + max);
+        }
+    }
+}
