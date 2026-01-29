e6e5527b89a4d196c5081df4a23d51f44bf1c214
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-01-29 22:32:22 +0530

    Add files via upload
    This is the print 1 to 10 Numbers program File.

---
+++ Print10Numbers.cs
@@ -0,0 +1,15 @@
+﻿namespace BasicCodes
+{
+    internal class Print10Numbers
+    {
+        public static void Numbers()
+        {
+           
+            Console.WriteLine("Print the 10 Number");
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
