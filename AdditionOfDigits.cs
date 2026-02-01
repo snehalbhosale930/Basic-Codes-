7c68701d08611ed98902e1f5d384b63b55816547
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-02-01 12:40:53 +0530

    Add files via upload
    This is Addition of Digits code file.

---
+++ AdditionOfDigits.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace BasicCodes
+{
+    internal class AdditionOfDigits
+    {
+        public static void AdditionOfDigit()
+        {
+            Console.WriteLine("Enter the number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            num = Math.Abs(num);
+
+           while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            Console.WriteLine("sum of digits:" + sum);
+            return;
+        }
+    }
+}
