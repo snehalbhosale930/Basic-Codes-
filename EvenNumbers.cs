95577b7daa74d55864612af2dfed7e7bf8275326
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-02-01 12:46:00 +0530

    Add files via upload
    This is Enter 1 to 10 Even Numbers code file.

---
+++ EvenNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicCodes
+{
+    internal class EvenNumbers
+    {
+        public static void EvenNumber()
+        {
+            for (int i = 1; i <= 10; i++) 
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
