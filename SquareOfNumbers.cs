2e0feda4d88361c5f88e3c9ab3cf965a8f860f82
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-02-01 12:49:45 +0530

    Add files via upload
    This is the 1 to 10 square of numbers code file.

---
+++ SquareOfNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace BasicCodes
+{
+    internal class SquareOfNumbers
+    {
+        public static void SquareOfNumber()
+        {
+
+            Console.WriteLine("Print Square of the Number");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i * i);
+            }
+        }
+    }
+}
