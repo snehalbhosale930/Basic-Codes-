59c7614044c257ad8f95f69262bef809d260108f
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-02-01 12:47:31 +0530

    Add files via upload
    This is  Multiplication of 2 integer without using * code file.

---
+++ MulOf2Int.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicCodes
+{
+    internal class MulOf2Int
+    {
+        public static void MultiOf2Int()
+        {
+            Console.WriteLine("Enter Number 1:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number 2:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            for (int i = 0; i < num2; i++)
+            {
+                result += num1;
+            }
+            Console.WriteLine("Product:" + result);
+        }
+    }
+}
