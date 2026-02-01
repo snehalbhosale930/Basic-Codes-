af0ca169982000ed54a60b5fc52e734b085d0ffd
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-02-01 12:42:16 +0530

    Add files via upload
    This is Count of Entered Digit code file.

---
+++ CountDigits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BasicCodes
+{
+    internal class CountDigits
+    {
+        public static void CountDigit()
+        {
+            Console.WriteLine("Enter the number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            while (num != 0)
+            {
+                num /= 10;
+                count++;
+            }
+            Console.WriteLine("Number of Digits: " + count);
+        }
+    }
+}
