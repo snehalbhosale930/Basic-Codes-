75e14d97d8188df6466b026a700510d0fa4f5e70
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-02-05 14:43:31 +0530

    Add files via upload
    In this file there is pattern printing codes file.

---
+++ PatternPrinting.cs
@@ -0,0 +1,100 @@
+﻿namespace BasicCodes
+{
+    internal class PatternPrinting
+    {
+        public static void print1to5Trangle()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void print1to5Stars()
+        {
+            int rows = 5;
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int k = rows - i; k > 0; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void HolloPyramid()
+        {
+            int rows = 5;
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int k = rows - i; k > 0; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i > 2 && i < rows)
+                    {
+                        if (j == 1 || j == i)
+                        {
+                            Console.Write("* ");
+                        }
+                        else
+                        {
+                            Console.Write("  ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("* ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void HolloAngle()
+        {
+            int rows = 5;
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int k = rows - i; k > 0; k--)
+                {
+                    Console.Write(" ");
+                }
+                 
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        if (i > 1 && i < rows)
+                        {
+                            if (j == 1 || i == rows)
+                            {
+                                Console.Write("*");
+                            }
+                            else
+                            {
+                                Console.Write("  ");
+                            }
+                        }
+                        else
+                        {
+
+                            Console.Write("* ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
