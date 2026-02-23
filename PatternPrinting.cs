bd904d1f5578690d461f530c480a0f3237927e63
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-02-23 15:16:01 +0530

    Update PatternPrinting.cs

--- PatternPrinting.cs
+++ PatternPrinting.cs
@@ -96,5 +96,94 @@
                 }
             }
         }
+        public static void RightHalfPyramid()
+        {
+            int rows = 5;
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int k = rows - i; k < 0; k--)
+                {
+                   Console.Write(" ");
+                }
+                 for (int j = 1; j <= i; j++)
+                 {
+                    Console.Write("* ");
+                  }
+                  Console.WriteLine();
+             }
+        }
+        public static void RevrighthalfPyramid()
+        {
+           int rows =5;
+           for (int i=1;i<=rows; i++)
+           {
+             for (int j = rows; j >= i; j--) 
+             {
+               Console.Write("*");
+            }
+              Console.WriteLine(" ");
+            }
+         }
+         public static void SquareFillpattern()
+         {
+           int rows = 6;
+           for (int i = 1; i <= rows; i++) 
+           {
+            for(int j=1; j<=rows; j++)
+            {
+              Console.Write(" *");
+             }
+             Console.WriteLine(" ");
+           }
+          }
+          public static void RevNoTriangle()
+          {
+           int rows = 4;
+           for(int i = 1; i<=rows; i++)
+           {
+            for(int k=1; k<i; k++)
+            {
+              Console.Write(" ");
+            }
+              for(int j=i; j<= rows; j++)
+              {
+                Console.Write(" " + j);
+              }
+               Console.WriteLine(" ");
+             }
+           }
+           public static void RhombusPattern()
+           {
+            int rows = 5;
+            for(int i = 1; i<= rows; i++)
+            {
+              for(int k = 0; k<i; k++)
+              {
+                Console.Write(" ");
+              }
+               for(int j=1; j<=rows; j++)
+               {
+                 Console.Write("*");
+                }
+                Console.WriteLine(" ");
+               }
+             }
+             public static void revHalfPyramid()
+             {
+              int rows = 5;
+              for(int i=rows;i>=1; i--)
+              {
+               for(int k=0; k < rows - i; k++)
+               {
+                Console.Write(" ");
+                }
+                for(int j=1; j<=i; j++)
+                {
+                Console.Write("*");
+                }
+                Console.WriteLine(" ");
+              }
+            }
+        }
     }
-}
+
